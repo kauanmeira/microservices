d1176fde07305295f7d2dcbf684dc51e1aec8286
kauan.meira <kauangabriel.meira123@gmail.com>
2023-05-19 14:56:04 -0300

    hr-payroll implementation, mock

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,4 +1,13 @@
 package com.example.hrpayroll.services;
 
+import com.example.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentService {
+    //Mock
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Kauan", 200.0, days);
+
+    }
 }
